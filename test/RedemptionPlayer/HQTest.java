da4691a8418b5447060c0c63457100a1f15aca4d
Sam Youngs <youngs3@pdx.edu>
2020-11-03 19:24:57 -0800

    Initial tests

---
+++ test/RedemptionPlayer/HQTest.java
@@ -0,0 +1,34 @@
+package RedemptionPlayer;
+
+import battlecode.common.*;
+import battlecode.common.RobotController;
+import javafx.beans.binding.When;
+import org.junit.Assert;
+import org.junit.Test;
+import RedemptionPlayer.RobotPlayer;
+import org.mockito.Mockito;
+
+		import static org.junit.Assert.assertEquals;
+		import static org.mockito.Mockito.when;
+		import static org.junit.Assert.assertNotEquals;
+
+
+public class RobotPlayerTest {
+	//static RobotController rc;
+
+
+	@Test
+	public void RobotCreationTest() {
+			RobotPlayer robotPlayer1 = Mockito.mock(RobotPlayer.class);
+			RobotPlayer robotPlayer2 = Mockito.mock(RobotPlayer.class);
+			Assert.assertNotSame("RobotPlayer1 is the same as RobotPlayer2",robotPlayer1, robotPlayer2);
+	}
+
+	@Test
+	public void testGetType() {
+		RobotController rc = Mockito.mock(RobotController.class);
+		when(rc.getType()).thenReturn(RobotType.MINER);
+		assertEquals(rc.getType(), RobotType.MINER);
+	}
+
+}
