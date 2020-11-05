b7ded1fce7b05d4535f0da47ca9faca05737a0ca
Sam Youngs <youngs3@pdx.edu>
2020-11-04 21:18:22 -0800

    Merging new HQ tests

--- test/RedemptionPlayer/HQTest.java
+++ test/RedemptionPlayer/HQTest.java
@@ -3,12 +3,6 @@ package RedemptionPlayer;
 import battlecode.common.*;
 import org.junit.Assert;
 import org.junit.Test;
-<<<<<<< HEAD
-import org.mockito.Mockito;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-=======
 import RedemptionPlayer.RobotPlayer;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -17,7 +11,6 @@ import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.*;
->>>>>>> a2a7610991fc253411a3ca427d19a13b42cf0dee
 
 
 public class HQTest {
@@ -31,41 +24,6 @@ public class HQTest {
 	}
 
 	@Test
-<<<<<<< HEAD
-	public void BuildMinerTest() {
-		HQ hq1 = Mockito.mock(HQ.class);
-		hq1.rc = Mockito.mock(RobotController.class);
-		hq1.mapHeight = 30;
-		hq1.mapWidth=30;
-		MapLocation loc = new MapLocation(15,15);
-		when(hq1.rc.adjacentLocation(Direction.NORTH)).thenReturn(loc);
-		when(hq1.rc.getType()).thenReturn(RobotType.HQ);
-		when(hq1.rc.getTeamSoup()).thenReturn(100);
-		when(hq1.rc.getRoundNum()).thenReturn(2);
-		when(hq1.rc.isReady()).thenReturn(true);
-		when(hq1.rc.canBuildRobot(RobotType.MINER, Direction.NORTH)).thenReturn(true);
-		try {
-			assertTrue(hq1.tryBuild(RobotType.MINER, Direction.NORTH));
-		}
-		catch (GameActionException ex){
-			return;
-		}
-	}
-
-	@Test
-	public void sendHQloctest() {
-		HQ hq = Mockito.mock(HQ.class);
-		hq.rc = Mockito.mock(RobotController.class);
-		int[] message = new int[7];
-		when(hq.rc.canSubmitTransaction(message, 3)).thenReturn(true);
-		try {
-			hq.sendHqLoc(hq.rc.getLocation());
-			assertNotNull(hq.rc.getBlock(3));
-		}
-		catch (GameActionException ex) {
-			return;
-		}
-=======
 	public void testSendHQLocSuccess() throws GameActionException {
 		//Prepare test
 		int expectedX = 1;
@@ -135,7 +93,6 @@ public class HQTest {
 
 		//verify
 		assertEquals(hq.numMiners, 5);
->>>>>>> a2a7610991fc253411a3ca427d19a13b42cf0dee
 	}
 
 	@Test
