b9603d1b5ce07df3b12754d708dbbf119fdf43f2
Sam Youngs <youngs3@pdx.edu>
2020-11-24 20:57:34 -0800

    some failing tests for the moment

--- test/redemptionplayer/DesignSchoolTest.java
+++ test/redemptionplayer/DesignSchoolTest.java
@@ -28,7 +28,27 @@ public class DesignSchoolTest {
 		//Act
 		ds.run();
 		//Assert
-//		verify(rc).buildRobot(RobotType.LANDSCAPER, Direction.NORTH);
+		verify(rc).buildRobot(RobotType.LANDSCAPER, Direction.NORTH);
+	}
+
+	@Test
+	public void HaltLandScaper() throws GameActionException {
+		//Arrange
+		RobotController rc = Mockito.mock(RobotController.class);
+		Transaction tx = Mockito.mock(Transaction.class);
+		RobotInfo enemyHq = new RobotInfo(1001, Team.NEUTRAL, RobotType.HQ, 0,false,0,0,0,null);
+		RobotInfo [] infoArr = new RobotInfo[1];
+		int [] mess = {384392, 4};
+		when(tx.getMessage()).thenReturn(mess);
+		infoArr[0] = enemyHq;
+		when(rc.isReady()).thenReturn(true);
+		when(rc.getRoundNum()).thenReturn(5);
+		DesignSchool.landscaperCount = 1;
+		DesignSchool ds = new DesignSchool(rc);
+		//when(ds.nearbyEnemyRobot(any(RobotType.class))).thenReturn(false);
+		when(ds.checkHalt()).thenReturn(true);
+		//Act
+		ds.run();
 	}
 
 }
