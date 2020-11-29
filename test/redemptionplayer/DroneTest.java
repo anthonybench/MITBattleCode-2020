c85cdbdd47a1a8666afa8ab4b3d64c29657a0324
anthonybench <anthonybenchyep@gmail.com>
2020-11-28 21:17:53 -0800

    whywhywhy

--- test/redemptionplayer/DroneTest.java
+++ test/redemptionplayer/DroneTest.java
@@ -22,9 +22,26 @@ public class DroneTest {
         when(rc.senseNearbyRobots()).thenReturn(infoArr);
         when(rc.getTeamSoup()).thenReturn(200);
         when(rc.getLocation()).thenReturn(new MapLocation(1,1));
+        when(rc.getRoundNum()).thenReturn(601);
+        when(rc.getBlock(any(int.class))).thenReturn(new Transaction[0]);
+        when(rc.getTeam()).thenReturn(Team.A);
+//        when(rc.getLocation().thenReturn(new MapLocation(1,1)));
+        //
+        Drone.minerLoc = new MapLocation(1,1);
+        Drone.firstMinerID = 1;
+        Drone.pickUpID = -1;
+        Drone.pickUpType = RobotType.LANDSCAPER;
+        Drone.pickUpLocation = new MapLocation(5,5);
+        Drone.sameTeam = true;
+        Drone.nearestWater = new MapLocation (20,20);
+        Drone.moveToEnemyBaseTurn = 500;
+        Drone.attackTurn = 600;
+        Drone.enemyHqLoc = null;
+        Drone.hqLoc = new MapLocation(10,10);
 
+        Drone d = new Drone(rc);
+        when(d.nearbyEnemyRobot(RobotType.HQ)).thenReturn(true);
 
-       Drone d = new Drone(rc);
         d.run();
     }
 }
